d1a9e5238afc946d703c42064afb6f4e30cf0a29
aaronma300604 <aaronmayoralansias@gmail.com>
2025-05-29 19:27:22 +0200

    Created first training data

--- src/mcts/generate_data.py
+++ src/mcts/generate_data.py
@@ -1,6 +1,17 @@
 from data_generation import generate_data, save_data
+import os
 
 if __name__ == "__main__":
-    data = generate_data(num_games=10,UCT_depth=10)
-    save_data(data, filename='data/othello_train_data.npz')
+    
+    FILENAME = 'data/othello_train_data.npz'
+    if(os.path.exists(FILENAME)):
+        inpt = ""
+        while inpt.lower() != 'y' and inpt.lower() !='n':
+            inpt = input(f"File {FILENAME} already exists. Continuing will overwrite it. Are you sure whatever you are doing is worth it? (y/n): ")
+            if(inpt.lower() == 'n'):
+                print("Exiting without generating data.")
+                exit(0)
+
+    data = generate_data(num_games=100,UCT_depth=20)
+    save_data(data, filename=FILENAME)
     
